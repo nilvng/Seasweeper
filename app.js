8eea78fa9c756fddc8591572d053b8c5938c27f0
nilvng <55075292+nilvng@users.noreply.github.com>
2020-10-14 17:13:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
 // import Minesweeper_puzzle from "./puzzle";
 
-const cardPuzzle = document.querySelector(".puzzle");
-const modeOption = document.querySelector(".level-settings");
+const puzzleElement = document.querySelector(".puzzle");
+const levelOption = document.querySelector(".level-settings");
 
 class Minesweeper_puzzle {
-  constructor(width, height) {
+  constructor(width, height, noBomb) {
     this.puzzle = [];
     this.puzzle.length = width * height;
     this.width = width;
     this.height = height;
+    this.noBomb = noBomb;
   }
 
-  createPuzzle(noBomb = (this.width * this.height) / 3) {
+  createPuzzle() {
     this.puzzle.fill(0);
     let arr = [];
     let bomb = [];
     for (let i = 0; i < this.puzzle.length; i++) {
       arr.push(i);
     }
-    bomb = arr.sort(() => Math.random() - 0.5).slice(0, noBomb);
+    bomb = arr.sort(() => Math.random() - 0.5).slice(0, this.noBomb);
 
     for (let i = 0; i < bomb.length; i++) {
       let pos = bomb[i];
       this.puzzle[pos] = 100;
-      if (pos - this.width > 0) {
+      if (pos - this.width >= 0) {
         this.puzzle[pos - this.width] += 1;
       }
-      if (pos - this.width - 1 > 0) {
+      if (pos - this.width - 1 >= 0) {
         this.puzzle[pos - this.width - 1] += 1;
       }
-      if (pos - this.width + 1 > 0) {
+      if (pos - this.width + 1 >= this.width) {
         this.puzzle[pos - this.width + 1] += 1;
       }
 
@@ -43,13 +44,41 @@ class Minesweeper_puzzle {
         this.puzzle[pos + this.width + 1] += 1;
       }
     }
-
+    console.log(this.puzzle)
     return this.puzzle;
   }
-
+  
   getSquare(id) {
     return this.puzzle[id];
   }
 }
-const puzzle = new Minesweeper_puzzle(6, 6);
-cardPuzzle.innerHTML = puzzle.createPuzzle(15);
+
+class App {
+  constructor(contextElement,puzzleElement, scoreElement){
+    this.contextElement = contextElement
+    this.puzzleElement = puzzleElement
+    console.log(puzzleElement)
+    this.scoreElement = scoreElement
+  }
+
+  start(){
+    const width = 6
+    const height = 6
+    const noBomb = 7
+    
+    const puzzle = new Minesweeper_puzzle(width, height, noBomb);
+    puzzle.createPuzzle()
+  // Set the puzzle screen width match the level value
+  this.puzzleElement.style.gridTemplateRows = `repeat(${width}, ${40 / width}rem)`;
+  this.puzzleElement.style.gridTemplateColumns = `repeat(${height}, ${40 / height}rem)`;
+    for (var i = 0; i < width * height; i ++) {
+      let square = document.createElement('div')
+      console.log(puzzle.getSquare(i))
+      square.innerHTML = puzzle.getSquare(i)
+      this.puzzleElement.appendChild(square)
+    }
+  }
+}
+
+const app = new App("hello ",puzzleElement, levelOption)
+app.start()
